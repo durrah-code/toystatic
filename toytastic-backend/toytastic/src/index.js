6f1f521fb6a7ffbc8fd6e74087a5acdab50e8afb
durrah-code <desadurrah@gmail.com>
2025-01-14 03:05:51 +0800

    k

--- toytastic-backend/toytastic/src/index.js
+++ toytastic-backend/toytastic/src/index.js
@@ -1,13 +1,13 @@
 // src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';  // Import the App component
 
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App"; // Import your main App component
-
+// Render the App component inside the 'root' div in index.html
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById("root") // React will mount the App component here
+    document.getElementById('root')
 );
+
