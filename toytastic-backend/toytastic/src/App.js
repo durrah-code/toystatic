3a6043a2c1ed028e3c65846d9aaeeff924fb0a5b
durrah-code <desadurrah@gmail.com>
2025-01-13 23:13:33 +0800

    d

--- toytastic-backend/toytastic/src/App.js
+++ toytastic-backend/toytastic/src/App.js
@@ -1,15 +1,15 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './ProductList';
 
 const App = () => {
   return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          {/* You can add more routes like this for individual product pages */}
-        </Switch>
+          <Routes>
+              <Route path="/" element={<ProductList />} />
+              {/* You can add more routes like this for individual product pages */}
+          </Routes>
       </Router>
   );
 };
