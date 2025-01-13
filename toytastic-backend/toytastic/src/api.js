d6a4758f70b61c8bfeff9244b575e36c23d34bec
durrah-code <desadurrah@gmail.com>
2025-01-13 23:38:06 +0800

    update

--- toytastic-backend/toytastic/src/api.js
+++ toytastic-backend/toytastic/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://durrah-code.github.io/toystatic/',
 });
 
 export const getProducts = () => api.get('/products');
