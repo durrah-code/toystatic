b0b395b9c37c8ea27cc8deadb44c3edcc3073f2a
durrah-code <desadurrah@gmail.com>
2025-01-13 22:52:18 +0800

    h

--- toytastic-backend/toytastic/src/db.js
+++ toytastic-backend/toytastic/src/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'toytastic_db',
+    user: 'system',
+    password: 'durrah',
+    database: 'toytastic',
 });
 
 module.exports = pool.promise();
